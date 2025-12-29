a6213bfbfd420c300c766c2759570df0c5380f4a
uroojkhadim <uroojkhadim505@gmail.com>
2025-12-29 13:11:22 +0500

    commit

--- SMART_POS/lib/utils/theme.dart
+++ SMART_POS/lib/utils/theme.dart
@@ -39,12 +39,12 @@ class AppTheme {
         borderSide: const BorderSide(color: Color(AppConstants.secondaryColorValue), width: 2),
       ),
     ),
-    cardTheme: const CardTheme(
+    cardTheme: CardThemeData(
       elevation: 2,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(AppConstants.borderRadius),
       ),
-    ).copyWith(),
+    ),
   );
 
   static ThemeData darkTheme = ThemeData(
@@ -84,7 +84,7 @@ class AppTheme {
         borderSide: const BorderSide(color: Color(AppConstants.secondaryColorValue), width: 2),
       ),
     ),
-    cardTheme: CardTheme(
+    cardTheme: CardThemeData(
       elevation: 2,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(AppConstants.borderRadius),
